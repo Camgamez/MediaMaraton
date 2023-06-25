781e53e9bbff083cce122fd6bf76ca2398c2bb61
Ivan Camilo Gamez <camgamezc@gmail.com>
2023-06-24 23:59:11 -0500

    Integración UI
    En este comit se encuentra integrada la ventana de ConsultarClasificación. Sin embargo, el objeto de clasificación que realiza la consulta a la BD todavía no está instanciado.

--- Controller/pesentar.py
+++ Controller/pesentar.py
@@ -8,6 +8,8 @@ from View.mainwindow import Ui_MainWindow
 from View.CarreraForm import Ui_CarreraForm
 from View.ModuloAtleta import Ui_ModuloAtleta
 from View.ResultadoCarrera import Ui_ResultadoCarrera
+from View.ConsultaResultados import Ui_ConsultaResultado
+from model.main import Clasificacion
 import sys
 
 
@@ -22,6 +24,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.goToModuloAtleta)
         self.ui.pushButton_2.clicked.connect(self.goToActualizarResultado)
+        self.ui.pushButton_3.clicked.connect(self.goToConsultarResultado)
         self.ui.pushButton_4.clicked.connect(self.goToClasificados)
         self.ui.pushButton_5.clicked.connect(self.goToResultadoCarrera)
         self.ui.pushButton_6.clicked.connect(self.goToCarreraForm)
@@ -48,6 +51,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def goToClasificados(self):
         stack.setCurrentIndex(stack.currentIndex() + 5)
+
+    def goToConsultarResultado(self):
+        stack.setCurrentIndex(stack.currentIndex() + 6)
 
 
 class CarreraForm(QtWidgets.QMainWindow):
@@ -124,25 +130,61 @@ class Clasificados(QtWidgets.QMainWindow):
         stack.setCurrentIndex(stack.currentIndex() - 5)
 
 
+class ConsultarResultado(QtWidgets.QMainWindow):
+    """
+    Esta clase crea una tabla que busca la información guardada en la base de datos de la clasificación de la carrera.
+    para ello se debe instanciar un objeto de la clase Clasificación y se debe ejecutar la función consultar_carrera()
+    que nos devuelve una lista de tuplas, con esa lista de tuplas se llena de información la tabla.
+    Pille pez, esta es la parte importante, lo que falta es lo siguiente:
+    """
+
+    def __init__(self):
+        super(ConsultarResultado, self).__init__()
+        self.ui = Ui_ConsultaResultado()
+        self.ui.setupUi(self)
+        self.ui.tableWidget
+        self.ui.tableWidget.setRowCount(len(tabla)) # La tabla todavía no está instanciada a este lado.
+        self.ui.tableWidget.setColumnCount(6)
+        self.ui.pushButton.clicked.connect(self.goToMainWindow)
+        self.ui.pushButton_2.clicked.connect(self.porTiempos)
+
+    def goToMainWindow(self):
+        stack.setCurrentIndex(stack.currentIndex() - 6)
+
+    def porTiempos(self):
+        pass
+
+
+
+
+
+
 app = QtWidgets.QApplication([])
-stack = QtWidgets.QStackedWidget() # El stack contiene todas las ventanas
+"""
+El stack es una lista de objetos. 
+Cada objeto es una instanciación de una ventana diferente.   
+"""
+stack = QtWidgets.QStackedWidget()
+
+# A continuación están las ventanas instanciadas.
 application = MainWindow()
 carreraForm = CarreraForm()
 moduloAtleta = ModuloAtleta()
 resultadoCarrera = ResultadoCarrera()
 actualizarResultado = ActualizarResultado()
 clasificados = Clasificados()
+consulta = ConsultarResultado()
 
+# Aquí se agregan las ventanas en el stack
 stack.addWidget(application)
 stack.addWidget(carreraForm)
 stack.addWidget(moduloAtleta)
 stack.addWidget(resultadoCarrera)
 stack.addWidget(actualizarResultado)
 stack.addWidget(clasificados)
+stack.addWidget(consulta)
 
 stack.setFixedWidth(800)
 stack.setFixedHeight(500)
-
-
 stack.show()
 sys.exit(app.exec())
